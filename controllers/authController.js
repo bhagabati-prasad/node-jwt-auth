720c8a8b15dc6759d5238087ab7a56d12aea3774
Bhagabati Prasad <bhagabatiprasada@gmail.com>
2020-10-20 16:12:23 +0530

    first commit - signup user without hashing password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,31 +1,7 @@
 const User = require("../models/User");
 
-// handle errors
-const handleErrors = (err) => {
-  console.log(err.message, err.code);
-  let errors = { username: "", password: "" };
-
-  // duplicate error code
-  if (err.code === 11000) {
-    errors.email = "that email already registered";
-    return errors;
-  }
-
-  // validation errors
-  if (err.message.includes("user validation failed")) {
-    Object.values(err.errors).forEach(({ properties }) => {
-      errors[properties.path] = properties.message;
-    });
-  }
-  return errors;
-};
-
 module.exports.signup_get = (req, res) => {
   res.render("signup");
-};
-
-module.exports.login_get = (req, res) => {
-  res.render("login");
 };
 
 module.exports.signup_post = async (req, res) => {
@@ -35,11 +11,15 @@ module.exports.signup_post = async (req, res) => {
     const user = await User.create({ username, password });
     res.status(201).json(user);
   } catch (err) {
-    const errors = handleErrors(err);
-    res.status(400).json({ errors });
+    console.log(err);
+    res.status(400).send("error, usr not created");
   }
 };
 
+module.exports.login_get = (req, res) => {
+  res.render("signup");
+};
+
 module.exports.login_post = async (req, res) => {
-  res.send("login user");
+  res.render("signup");
 };
--- models/User.js
+++ models/User.js
@@ -1,28 +1,18 @@
 const mongoose = require("mongoose");
-const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, "Please enter an username"],
+    required: true,
     unique: true,
   },
-  // email: {
-  //   type: String,
-  //   required: [true, "Please enter an username"],
-  //   unique: true,
-  //   lowercase: true,
-  //   validate: [isEmail, "Please enter a valid email"],
-  // },
   password: {
     type: String,
-    required: [true, "Please enter an password"],
-    minlength: [2, "Minimum password length must be 2 characters"],
+    required: true,
+    minLength: 2,
   },
 });
 
-// connect the schema model with database
 const User = mongoose.model("user", userSchema);
 
-// export the module
 module.exports = User;
